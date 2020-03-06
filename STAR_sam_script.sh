7fc21b1b665b969b334f9655c6d37bc383018e9c
ConYel <yeles.konstantinos@gmail.com>
2020-03-06 16:38:14 +0100

    working script

--- STAR_sam_script.sh
+++ STAR_sam_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 FASTQ=$1 # input FASTQ file
          # <file>_trimmed.fastq.gz
 genome=$2 #genome folder
@@ -8,10 +9,13 @@ starOutDir=$4 #outdir
 
 str="${FASTQ##*/}"
 regex="${str%%.fastq.gz}"
-regex="${str%%.fq.gz}"
+regex="${regex%%.fq.gz}"
+regex="${regex%%.fasta.gz}"
+regex="${regex%%.fasta}"
+
 mkdir ${starOutDir}/${regex}_align
 echo "---- working on ${regex} file ----"
-STAR --genomeDir $genome --genomeLoad LoadAndKeep --readFilesIn ${FASTQ} --readFilesCommand zcat --runThreadN $threads --alignIntronMax 1 --outSAMattributes NH HI NM MD --outFilterMultimapNmax 100 --outReadsUnmapped Fastx --outFilterMismatchNmax 1 --outFilterMatchNmin 14 --outFileNamePrefix ${starOutDir}/${regex}_align
+STAR --genomeDir $genome --genomeLoad LoadAndKeep --readFilesIn ${FASTQ} --readFilesCommand zcat --runThreadN $threads --alignIntronMax 1 --outSAMattributes NH HI NM MD --outFilterMultimapNmax 100 --outReadsUnmapped Fastx --outFilterMismatchNmax 1 --outFilterMatchNmin 14 --outFileNamePrefix "${starOutDir}/${regex}_align/${regex}_"
 echo "---- now sorting ----"
 samtools sort -O bam -o ${starOutDir}/${regex}_align/${regex}_sorted.bam -@ ${threads} ${starOutDir}/${regex}_align/${regex}_Aligned.out.sam
 echo "---- now making the fastq ----"
@@ -19,4 +23,4 @@ samtools fastq -F 4 -@ ${threads} ${starOutDir}/${regex}_align/${regex}_sorted.b
 echo "---- removing not sorted sam ----"
 rm ${starOutDir}/${regex}_align/${regex}_Aligned.out.sam
 echo "---- making sorted sam for protrac ----"
-samtools view -h -@ ${threads} -o ${starOutDir}/${regex}_align/${regex}_sorted.sam in.bam ${starOutDir}/${regex}_align/${regex}_sorted.bam
+samtools view -h -@ ${threads} -o ${starOutDir}/${regex}_align/${regex}_sorted.sam ${starOutDir}/${regex}_align/${regex}_sorted.bam
